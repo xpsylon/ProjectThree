d2c1ee70f3847873c3c371b8189dada6c6fb93ee
xpsylon <xxxpsylon@gmail.com>
2023-06-15 23:05:24 +0200

    last of the day

--- ProjectThree/settings.py
+++ ProjectThree/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'blog.apps.BlogConfig', #agregamos la clase BlogConfig de blog.apps (herencia de AppsConfig q se crea automaticamente al crear la app blog)
     'django.contrib.admin',
     'django.contrib.auth',
