53bd13634efb1643dda7d2fcbc1f0aa85c741409
xpsylon <xxxpsylon@gmail.com>
2023-06-17 20:10:37 +0200

    kilombo atomico

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def registro(request):
     #This line defines a function called registro. The request parameter is a standard Django request object 
@@ -8,13 +9,17 @@ def registro(request):
     #This line checks to see if the request method is POST. If it is, then the user has submitted the registration form.
         form = UserCreationForm(request.POST)
     #This line creates a new UserCreationForm instance and populates it with the data that the user submitted in the registration form.
-        if form.is_valid:
+        if form.is_valid():
             username = form.cleaned_data.get('username')
+            messages.success(request, f'Cuenta creada para {username}!')
+            return redirect('blog-home') #redirige a blog-home una vez que el formulario se envio correctamente.
     else:
-        form = UserCreationForm()
+        form = UserCreationForm() 
     #This line is executed if the request method is not POST. In this case, the user has not yet submitted the registration form, 
     #so we need to create a new, empty UserCreationForm instance.
 
     return render(request, 'users/register.html', {'formulario': form})
     #This line renders the users/register.html template, passing it the form object as an argument. 
     #The users/register.html template will then display the registration form to the user.
+
+    #CHECK https://docs.djangoproject.com/en/4.2/topics/forms/
