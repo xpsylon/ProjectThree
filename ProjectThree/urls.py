f224439c6e80a4770cee799d7eb274af8048bb67
xpsylon <xxxpsylon@gmail.com>
2023-06-19 19:48:02 +0200

    a

--- ProjectThree/urls.py
+++ ProjectThree/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', user_views.registro, name='registro'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'entrar')
 
 ]
