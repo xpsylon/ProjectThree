bfbd0c2ab7e8467f1cba63809eab8bffa7a6a4da
xpsylon <xxxpsylon@gmail.com>
2023-06-23 10:58:13 +0200

    cap 9

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 #By default Django Users dont have the option to upload a profile picture. So we'll create a Profiles model to add this functionality.
 #It will have a ONE TO ONE relationship with the Users model. Always remember, a model is a table.
@@ -14,6 +15,16 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
         #output "eekemony Profile"
+    
+    def save(self):
+        super().save() #we put the save method of the parent class
+        #one method for resizing images:
+        imagen = Image.open(self.image.path)
+        if imagen.height > 300 or imagen.width > 300:
+            peso_max = (300, 300) #tupla
+            imagen.thumbnail(peso_max) #thumbnail metodo de PIL
+            imagen.save(self.image.path)
+
 
 #After creating a new model (table), dont forget to run migrations.
 #But with images, you'll first need to install Pillow.
