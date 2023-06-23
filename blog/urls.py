f01371f92c3ae29ba4712eed82b2641e1ab1dca3
xpsylon <xxxpsylon@gmail.com>
2023-06-23 16:08:54 +0200

    cap 10. class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from .views import PostListView
 from . import views
 
 #lista que contiene funciones
 urlpatterns = [
-    path('', views.home, name='casa-blog'),
+    #path('', views.home, name='casa-blog'), #LA ANTIGUA FUNCTION-BASED VIEW
+    path('', PostListView.as_view(), name='casa-blog'),
     path('about/', views.about,  name='sobre-el-blog'),
 ]
-
+#we get error template doesn exist. By convention, Django searches for:
+#<app>/<model>_<viewtype>.html
+#blog/post_list.html
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse #dont need it, we are not using it
+#from django.http import HttpResponse #dont need it, we are not using it
+from django.views.generic import ListView #for ListView type of class based view.
 from .models import Post
 
 # Create your views here.
@@ -7,10 +8,17 @@ def home(request):
     #creamos un diccionario usando como valor la variable posts. En el html template se loopea por la clave y da como respuesta el valor. El valor es la lista
     #de diccionarios.
     context = {
-        'cosas': Post.objects.all()
+         'cosas': Post.objects.all()
     }
-    #return HttpResponse('<h1>Blog Home</h1>')
     return render(request, 'blog/home.html', context)
+
+#creating a class-based view (instead of function-based view) from ListView type:
+class PostListView(ListView):
+    #todas estas variables son con nombres predefinidos por la clase madre ListView
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'cosas' #porque sino por default las ListViews buscan loopear por object_list
+    ordering = ['-date_posted'] #ordebar de fecha mas nueva a mas vieja (con el signo -)
 
 def about(request):
     #a este le agregamos el titulo como tercer argumento
