b6952ce77047ff0ac70453aa09657fe5c309213c
xpsylon <xxxpsylon@gmail.com>
2023-06-30 14:47:18 +0200

    last password reset

--- ProjectThree/urls.py
+++ ProjectThree/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     #template_name da la ruta donde debe buscar los templates. Por default busca en registration/login.html...
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'entrar'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name= 'salir'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
          name='password_reset_confirm'),
