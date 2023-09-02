3668cfbca66addeca151b5cf3a27c468ce2b9f83
xpsylon <xxxpsylon@gmail.com>
2023-09-02 11:09:34 +0200

    no comments

--- ProjectThree/urls.py
+++ ProjectThree/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+#estas auth views no hay que crearlas, ya vienen en el sistema:
 from django.contrib.auth import views as auth_views
 #STATIC FILED DEVELOPMENT MODE (DEBUG TRUE):
 from django.conf import settings
@@ -31,10 +32,14 @@ urlpatterns = [
     #template_name da la ruta donde debe buscar los templates. Por default busca en registration/login.html...
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'entrar'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name= 'salir'),
+    #despues de cliquear forgot password? te manda aca para q introduzcas tu email:
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    #te sale un mensaje que te mandaron un mail con un link para cambiar el password:
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    #pinchas en el link que recibis en el email y te lleva a poner el nuevo pswd y confirmarlo:
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
          name='password_reset_confirm'),
+    #mensaje de exito de que el password ha sido reseteado:         
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
          name='password_reset_complete'),
 
